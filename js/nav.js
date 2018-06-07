0a9a338672d7c27ef7c9c439820b0a8a6635a1a2
xjwyk <37682862+xjwyk@users.noreply.github.com>
2018-06-07 17:28:39 +0800

    Add files via upload
    update cover

--- js/nav.js
+++ js/nav.js
@@ -7,7 +7,6 @@
 
 $(function() {
     var liWidth = $('.main-right #main-right-nav li').width();
-
     $('.main-right #main-right-nav li').hover(function() {
         var index = $(this).index();
         $(this).addClass('on').siblings().removeClass('on');
@@ -15,22 +14,28 @@ $(function() {
     }, function() {
         $(this).find('.second').fadeOut(300);
     });
-});
-
-function change() {
-    var divDisp = document.getElementById("search").style.display;
-    if (divDisp == "block") {
-        document.getElementById("search").style.display = "none";
-    } else {
-        document.getElementById("search").style.display = "block";
-    }
-}
-
-$("#search-btn").click(function() {
-    var txt = "请输入";
-    window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.input, {
-        onOk: function(v) {
-            console.log(v);
-        }
+    var currentDate = "21";
+    $("span[id='read-count']").each(function() {
+        $(this).text(currentDate);
+    });
+    $("span[id='unread-count']").each(function() {
+        $(this).text(currentDate);
+    });
+    var len = $(".collect-left-ul li").length;
+    var index;
+    $(".collect-left-ul li").mousedown(function() {
+        var index = $(".collect-left-ul li").index(this);
+        show(index);
+    }).eq(0).mousedown();
+    $("#search-btn").click(function() {
+        var text = $("#search-text").val();
+        console.log(text);
     });
 });
+
+function show(index) {
+    $(".collect-right-ul li").eq(index).show().siblings('li').hide();
+    $(".collect-left-ul li").css("background", "#fff").eq(index).css("background", "#5a9ad7");
+    $(".collect-left-ul li div a").css("color", "#5a9ad7").eq(index).css("color", "#fff");
+    $(".collect-left-ul li div span").css("color", "#5a9ad7").eq(index).css("color", "#fff");
+}
