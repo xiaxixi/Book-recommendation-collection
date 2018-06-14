79fd418110a2a17d70f4c302cbbc9d3bdd9a1060
Xuan0216 <849651526@qq.com>
2018-06-14 22:44:34 +0800

    update schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,59 +1,56 @@
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 2018-06-14 16:41:39
+-- 服务器版本： 5.6.17
+-- PHP Version: 5.5.12
 
--- -----------------------------
--- Table structure for userTable
--- -----------------------------
-DROP TABLE IF EXISTS `userTable`;
-CREATE TABLE `userTable`  (
-  `user_id` int(15) NOT NULL AUTO_INCREMENT COMMENT '用户编号',
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `password` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户密码',
-  `favourite_type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户最喜爱的书籍类型',
-  PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- ----------------------------
--- Records of userTable
--- ----------------------------
-INSERT INTO `userTable` VALUES (1, 'abc', '123','math');
-INSERT INTO `userTable` VALUES (2, 'def', '456','computer');
 
--- -----------------------------
--- Table structure for bookTable
--- -----------------------------
-DROP TABLE IF EXISTS `bookTable`;
-CREATE TABLE `bookTable`  (
-  `book_id` int(50) NOT NULL AUTO_INCREMENT COMMENT '书籍编号',
-  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '书籍名称',
-  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '书籍作者',
-  `cover_addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '书籍图片存放地址',
-  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '书籍类型',
-  `star_level` int(5) NOT NULL COMMENT '推荐星级',
-  `intro` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '书籍简介',
-  `collect_num` int(50) NOT NULL COMMENT '标记是否收藏',
-  PRIMARY KEY (`book_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 33 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
--- --------------------
--- Records of bookTable
--- --------------------
-INSERT INTO `bookTable` VALUES (1, 'HTml5与css3基础教程', 'Elizabeth Castro', 'images/book_1_cover.png', 'computer', 4, '讲解html和css入门知识的经典畅销书，全面系统的讲解html5和css的基础知识以及实际运用技术', 0);
+--
+-- Database: `login`
+--
 
--- ------------------------------------
--- Table structure for userCollectTable
--- ------------------------------------
-DROP TABLE IF EXISTS `userCollectTable`;
-CREATE TABLE `userCollectTable`  (
-  `user_id` int(15) NOT NULL COMMENT '用户编号',
-  `book_id` int(50) NOT NULL COMMENT '图书编号',
-  `read_state` int(1) NOT NULL COMMENT '标记是否已读'
-) ;
+-- --------------------------------------------------------
 
--- ---------------------------
--- Records of userCollectTable
--- ---------------------------
-INSERT INTO `userCollectTable` VALUES (1, 1, 1);
-INSERT INTO `userCollectTable` VALUES (1, 2, 0);
+--
+-- 表的结构 `user`
+--
+-- 创建时间： 2018-06-14 14:40:31
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_name` varchar(24) CHARACTER SET utf8mb4 NOT NULL COMMENT '用户名',
+  `password` varchar(24) NOT NULL COMMENT '密码',
+  `favourite_type` varchar(50) DEFAULT NULL COMMENT '书籍喜好',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+
+--
+-- 插入之前先把表清空（truncate） `user`
+--
+
+TRUNCATE TABLE `user`;
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `password`, `favourite_type`) VALUES
+(1, 'admin', '123456', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
