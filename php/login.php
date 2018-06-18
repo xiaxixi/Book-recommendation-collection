938589cf30e5f9d3c29fddaab265247acb098be8
Xuan0216 <849651526@qq.com>
2018-06-18 22:17:47 +0800

    update login.php

--- php/login.php
+++ php/login.php
@@ -1,14 +1,24 @@
 <?php 
-$link = mysqli_connect('127.0.0.1', 'root', '', 'login');
+
+/**
+ * 
+ * @authors ChenMingxuan (849651526@qq.com)
+ * @date    2018-06-06 09:21:21
+ * @version $Id$
+ */
+
+header("Content-type: text/html; charset=utf-8"); 
+$link = mysqli_connect('127.0.0.1', 'root', '', 'brc');
 if (!$link){
     echo"<script>alert('数据库连接失败！')</script>";
 }else {
     if (isset($_POST['submit'])){
-        $query = "select * from user where user_name = '{$_POST['name']}' and pwd = '{$_POST['pwd']}'";
+        $query = "select * from user where name = '{$_POST['name']}' and pwd = '{$_POST['pwd']}'";
         $result = mysqli_query($link, $query);
         if (mysqli_num_rows($result) == 1){
-            header("Location:../html/user-center.html");
+            header("Location:../index.html");
         }
     }
 }
+mysqli_close($link);
 ?>
