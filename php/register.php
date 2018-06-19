d20059bd99200db56433c13a45b10402a85be891
Xuan0216 <849651526@qq.com>
2018-06-19 16:41:15 +0800

    update register.php

--- php/register.php
+++ php/register.php
@@ -8,10 +8,10 @@
 
     header("Content-type: text/html; charset=utf-8"); 
 
-    $servername ="127.0.0.1";
-    $username   ="root";
-    $password   ="";
-    $database   ="brc";
+    $servername = "127.0.0.1";
+    $username   = "root";
+    $password   = "";
+    $database   = "brc";
 
     if (isset($_POST['submit'])){
         $name = $_POST["name"];  
