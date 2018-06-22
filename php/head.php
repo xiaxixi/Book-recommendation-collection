609255ef0a09f6c8c622f1c0c591cc4d183c3b4f
xjwyk <37682862+xjwyk@users.noreply.github.com>
2018-06-22 10:06:57 +0800

    Update head.php
    hah

--- php/head.php
+++ php/head.php
@@ -11,21 +11,18 @@ $link = mysqli_connect('127.0.0.1', 'root', 'okfCRv0q', 'brc');
 mysqli_query($link, "SET NAMES 'UTF8'");
 if (!$link){
     echo"<script>alert('数据库连接失败！')</script>";
-}else{
-  	$name = $_SESSION['name'];
+}else{$name = $_SESSION['name'];
 	$query = "select image_addr from `user` where name = '$name'";
-
     $result = mysqli_query($link, $query);
-    
     if($result && mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        $img = $row['image_addr'];
-        $str = '<div class="head-box col-md-2 col-xs-2 col-lg-2">
-          			<img src='.$img.' href="user-center.html" id="head-user-image">
-          			<a class="head-user-name" href="user-center.html" id="head-user-name">'.$name.'</a>
-        		</div>';
-        echo $str;
-      }
+        while ($row = mysqli_fetch_assoc($result)) {
+            $img = $row['image_addr'];
+            $str = '<div class="head-box col-md-2 col-xs-2 col-lg-2">
+          			    <img src='.$img.' href="user-center.html" id="head-user-image">
+          			    <a class="head-user-name" href="user-center.html" id="head-user-name">'.$name.'</a>
+        		    </div>';
+            echo $str;
+        }
     }
 }
 ?>
